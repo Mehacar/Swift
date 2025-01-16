4dd5e148d47ec0d51dd1e072c7e6d217c47c0a1f
Mehmet Tunahan Acar <tunahanacar849@gmail.com>
2025-01-16 15:50:58 +0300

    SwiftBasics file resolved and added missing files

---
+++ Swiftbasics/SwiftBasics.playground/Contents.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var userName = "James"
+print(userName)
+
+userName.append("t")
+userName.lowercased()
+
+// variables
+
+var number = 45
+var number2 = 38.0   // double
+var number3: Float = 23.4 // float
+
+let pi = 3.14 // use let for nonconvertable variables
+
+// boolean
+
+var boolstatement = false
+boolstatement = true
+
+// String
+
+let myStr : String = "40"
+let stringNumber : String = String(30)
+
+// define
+
+let str : String
+
+// initilation
+
+str = "Hello"
+
+let myUpperVariable = str.uppercased()
+print(myUpperVariable)
+print(str)
+
+
+
+
