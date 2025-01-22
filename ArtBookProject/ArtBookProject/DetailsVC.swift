87292ba4c2b6683a9285754749d110a67963f234
Mehmet Tunahan Acar <tunahanacar849@gmail.com>
2025-01-22 23:43:24 +0300

    Started ArtbookProject(ok the image chosing)

--- ArtBookProject/ArtBookProject/DetailsVC.swift
+++ ArtBookProject/ArtBookProject/DetailsVC.swift
@@ -6,24 +6,116 @@
 //
 
 import UIKit
+import CoreData
+import PhotosUI
 
-class DetailsVC: UIViewController {
+class DetailsVC: UIViewController, PHPickerViewControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var artistTextField: UITextField!
+    @IBOutlet weak var yearTextFiled: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.save, target: self, action: #selector(saveButton))
 
-        // Do any additional setup after loading the view.
+        // Recognizers
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyword))
+        view.addGestureRecognizer(gestureRecognizer)
+        
+        imageView.isUserInteractionEnabled = true
+        let imageTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        imageView.addGestureRecognizer(imageTapRecognizer)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func selectImage() {
+            var configuration = PHPickerConfiguration()
+            configuration.selectionLimit = 1 // 0 - unlimited | 1 - one image
+        configuration.filter = .images   // just show images
+            
+            let picker = PHPickerViewController(configuration: configuration)
+            picker.delegate = self
+            present(picker, animated: true)
     }
-    */
+    
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            // close the picker
+            picker.dismiss(animated: true)
+            
+            // take the chosen image
+            guard let result = results.first else { return }
+            
+            result.itemProvider.loadObject(ofClass: UIImage.self) { [weak self] reading, error in
+                if let error = error {
+                    print("Error loading image: \(error.localizedDescription)")
+                    return
+                }
+                
+                if let image = reading as? UIImage {
+                    // Do it UI updates in main thread
+                    DispatchQueue.main.async {
+                        self?.imageView.image = image
+                    }
+                }
+            }
+        }
+    
+
+//        @objc func selectImage(){
+//            let picker = UIImagePickerController()
+//            picker.delegate = self
+//            picker.sourceType = .photoLibrary     // this method is old look here again ! (looked)
+//            picker.allowsEditing = true //image editing tool
+//            present(picker, animated: true, completion: nil)
+//        }
+//
+//    
+//        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//            imageView.image = info[.originalImage] as? UIImage //assign image variable type
+//            self.dismiss(animated: true, completion: nil)
+//        }
+    
+    @objc func hideKeyword() {
+        view.endEditing(true)
+    }
+
+    @objc func saveButton() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newPainting = NSEntityDescription.insertNewObject(forEntityName: "Paintings", into: context)
+        
+        //Attributes
+        
+        newPainting.setValue(nameTextField.text!, forKey: "name")
+        newPainting.setValue(artistTextField.text!, forKey:  "artist")
+        newPainting.setValue(UUID(), forKey: "id")
+        
+        
+        if let year = Int(yearTextFiled.text!) {
+            newPainting.setValue(year, forKey: "year")
+        }
+        
+        let data = imageView.image?.jpegData(compressionQuality: 0.5)
+        newPainting.setValue(data, forKey: "image")
+        
+        do {
+            try context.save()
+            print("success")
+        } catch {
+            print("error")
+        }
+        
+        
+        
+        
+        
+    }
+    
 
 }
--- ArtBookProject/ArtBookProject/ViewController.swift
+++ ArtBookProject/ArtBookProject/ViewController.swift
@@ -6,12 +6,74 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
+    @IBOutlet weak var tableView: UITableView!
+    var nameArray: [String] = []
+    var idArray: [UUID] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //you can use navigationController?.navigationBar.topItem?.rightBarButtonItem 
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        getData()
+        
+        
+    }
+    
+    func getData() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+        fetchRequest.returnsObjectsAsFaults = false
+        // load the data quickly - fast but not efficiently || true -> usage memory efficiently(for big data)
+        
+    
+        do {
+            let results = try context.fetch(fetchRequest)
+            
+            for result in results as! [NSManagedObject]{
+                if let name = result.value(forKey: "name") as? String {
+                    self.nameArray.append(name)
+                }
+                
+                if let id = result.value(forKey: "id") as? UUID {
+                    self.idArray.append(id)
+                }
+                self.tableView.reloadData()
+                
+            }
+
+        } catch {
+            print("error")
+        }
+            
+    }
+
+    
+    @objc func addButtonClicked() {
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArray.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        var content = cell.defaultContentConfiguration()
+        content.text = nameArray[indexPath.row]
+        cell.contentConfiguration = content
+        return cell
     }
 
 
