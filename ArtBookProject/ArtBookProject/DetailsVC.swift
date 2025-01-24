78f0972fb9aafce3830f6074279a7b902247af40
Mehmet Tunahan Acar <tunahanacar849@gmail.com>
2025-01-24 23:46:09 +0300

    Art Book Project Complated

--- ArtBookProject/ArtBookProject/DetailsVC.swift
+++ ArtBookProject/ArtBookProject/DetailsVC.swift
@@ -12,19 +12,63 @@ import PhotosUI
 class DetailsVC: UIViewController, PHPickerViewControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
-
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var artistTextField: UITextField!
     @IBOutlet weak var yearTextFiled: UITextField!
     
+    var chosenPainting = ""
+    var chosenPaintingId : UUID?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.save, target: self, action: #selector(saveButton))
 
+        if chosenPainting != "" {
+            //coreData
+            
+            navigationItem.rightBarButtonItem?.isHidden = true // save button
+            view.backgroundColor = .lightGray
+            
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+            let idString = chosenPaintingId?.uuidString
+            fetchRequest.predicate = NSPredicate(format: "id = %@", idString!)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do {
+                let results = try context.fetch(fetchRequest)
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject] {
+                        if let name = result.value(forKey: "name") as? String {
+                            nameTextField.text = name
+                        }
+                        if let artist = result.value(forKey: "artist") as? String {
+                            artistTextField.text = artist
+                        }
+                        if let year = result.value(forKey: "year") as? Int {
+                            yearTextFiled.text = String(year)
+                        }
+                        if let imageData = result.value(forKey: "image") as? Data {
+                            let image = UIImage(data: imageData)
+                            imageView.image = image
+                        }
+                    }
+                }
+            } catch {
+                print("error")
+            }
+//            let stringUUID = chosenPaintingId!.uuidString
+//            print(stringUUID)
+        } else {
+            navigationItem.rightBarButtonItem?.isEnabled = false
+            navigationItem.rightBarButtonItem?.isHidden = false
+        }
+        
         // Recognizers
         
-        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyword))
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         view.addGestureRecognizer(gestureRecognizer)
         
         imageView.isUserInteractionEnabled = true
@@ -36,7 +80,7 @@ class DetailsVC: UIViewController, PHPickerViewControllerDelegate {
     @objc func selectImage() {
             var configuration = PHPickerConfiguration()
             configuration.selectionLimit = 1 // 0 - unlimited | 1 - one image
-        configuration.filter = .images   // just show images
+            configuration.filter = .images   // just show images
             
             let picker = PHPickerViewController(configuration: configuration)
             picker.delegate = self
@@ -44,12 +88,13 @@ class DetailsVC: UIViewController, PHPickerViewControllerDelegate {
     }
     
     func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            
+            navigationItem.rightBarButtonItem?.isEnabled = true //save button
             // close the picker
             picker.dismiss(animated: true)
             
             // take the chosen image
             guard let result = results.first else { return }
-            
             result.itemProvider.loadObject(ofClass: UIImage.self) { [weak self] reading, error in
                 if let error = error {
                     print("Error loading image: \(error.localizedDescription)")
@@ -66,6 +111,7 @@ class DetailsVC: UIViewController, PHPickerViewControllerDelegate {
         }
     
 
+
 //        @objc func selectImage(){
 //            let picker = UIImagePickerController()
 //            picker.delegate = self
@@ -80,7 +126,7 @@ class DetailsVC: UIViewController, PHPickerViewControllerDelegate {
 //            self.dismiss(animated: true, completion: nil)
 //        }
     
-    @objc func hideKeyword() {
+    @objc func hideKeyboard() {
         view.endEditing(true)
     }
 
@@ -111,10 +157,8 @@ class DetailsVC: UIViewController, PHPickerViewControllerDelegate {
             print("error")
         }
         
-        
-        
-        
-        
+        NotificationCenter.default.post(name: NSNotification.Name("newData"), object: nil)
+        self.navigationController?.popViewController(animated: true)
     }
     
 
--- ArtBookProject/ArtBookProject/ViewController.swift
+++ ArtBookProject/ArtBookProject/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet weak var tableView: UITableView!
     var nameArray: [String] = []
     var idArray: [UUID] = []
+    var selectedPainting = ""
+    var selectedPaintingId : UUID?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,10 +27,17 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         getData()
         
-        
     }
     
-    func getData() {
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(getData), name: NSNotification.Name(rawValue: "newData"), object: nil)
+    }
+    
+    @objc func getData() {
+        
+        nameArray.removeAll(keepingCapacity: false)
+        idArray.removeAll(keepingCapacity: false)
+        
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
         
@@ -39,19 +48,17 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
         do {
             let results = try context.fetch(fetchRequest)
-            
-            for result in results as! [NSManagedObject]{
-                if let name = result.value(forKey: "name") as? String {
-                    self.nameArray.append(name)
+            if results.count > 0 {
+                for result in results as! [NSManagedObject] {
+                    if let name = result.value(forKey: "name") as? String {
+                        self.nameArray.append(name)
+                    }
+                    if let id = result.value(forKey: "id") as? UUID {
+                        self.idArray.append(id)
+                    }
+                    self.tableView.reloadData()
                 }
-                
-                if let id = result.value(forKey: "id") as? UUID {
-                    self.idArray.append(id)
-                }
-                self.tableView.reloadData()
-                
             }
-
         } catch {
             print("error")
         }
@@ -60,6 +67,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     
     @objc func addButtonClicked() {
+        selectedPainting = ""
         performSegue(withIdentifier: "toDetailsVC", sender: nil)
     }
     
@@ -74,6 +82,57 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         content.text = nameArray[indexPath.row]
         cell.contentConfiguration = content
         return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailsVC" {
+            let destinationVC = segue.destination as! DetailsVC
+            destinationVC.chosenPainting = selectedPainting
+            destinationVC.chosenPaintingId = selectedPaintingId
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedPainting = nameArray[indexPath.row]
+        selectedPaintingId = idArray[indexPath.row]
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            let idString = idArray[indexPath.row].uuidString
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+            fetchRequest.predicate = NSPredicate(format: "id = %@", idString)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do {
+            let results = try context.fetch(fetchRequest)
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject] {
+                        if let id = result.value(forKey: "id") as? UUID {
+                            if id == idArray[indexPath.row] {
+                                context.delete(result)
+                                nameArray.remove(at: indexPath.row)
+                                idArray.remove(at: indexPath.row)
+                                self.tableView.reloadData()
+                                do {
+                                    try context.save()
+                                } catch {
+                                    print("error")
+                                }
+                                break
+                            }
+                        }
+                    }
+                }
+            } catch {
+                print("error")
+            }
+            
+            
+        }
     }
 
 
