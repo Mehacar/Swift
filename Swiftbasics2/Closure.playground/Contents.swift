47e3aa59fe9309e2400f0851d7bbba4834464a1f
Mehmet Tunahan Acar <tunahanacar849@gmail.com>
2025-07-30 15:19:31 +0300

    closure basics

--- Swiftbasics2/Closure.playground/Contents.swift
+++ Swiftbasics2/Closure.playground/Contents.swift
@@ -1,3 +1,16 @@
 import UIKit
 
 var greeting = "Hello, playground"
+// write sum func,
+//write calculate taking sum as parameter
+
+func mysum(num1: Int, num2: Int) -> Int{
+    return num1 + num2
+}
+
+func calculate(num1: Int, num2: Int, myfunction: (Int, Int) -> Int) -> Int{
+    return myfunction(num1,num2)
+}
+
+calculate(num1: 4,num2: 11, myfunction: mysum)
+
